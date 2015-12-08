5af808642f8918bc62ca013ee8b30c5e977fd0ad
Makho Kevlishvili <maxuna15@mail.ru>
2015-12-08 16:55:42 +0400

    Create index.php
    მთავარი გვერდი დამუშავების პროცესში

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<!--
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+-->
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="/bower_components/material-design-lite/material.min.css">
+  <script src="/bower_components/material-design-lite/material.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body class="fullbleed vertical layout">
+<div class="header">
+      <div>ჰეადერის კონტენტი წავა აააააქ ჰაჰაჰაჰაჰჰა</div>
+  <div class="nav">
+  <!-- <a href="/"><span class="text">მთავარი</span></a> -->
+  <!-- Accent-colored raised button with ripple -->
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+  მთავარი
+</button>
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+  ჩვენს შესახებ
+</button>
+  <!-- <a href="/about-us.html">ჩვენს შესახებ</a> -->
+  <a href="allcalc.html">კალკულატორები</a>
+  <a href="#">კონვერტერები</a>
+  <a href="#">რეგისტრაცია/ავტორიზაცია</a>
+  <a href="#">კონტაქტი</a>
+</div>
+
+</div>
+<div class="container">
+	
+
+      <div>კონტენტის კონტენტი წავა ააააააქ ჰაჰაჰაჰაჰჰაჰა</div>
+
+
+
+
+
+</div>
+<div class="footer">
+	<center><span id="footer">დიადი და დიდებული ავტორის ინფო წავა ააააააქ ჰაჰაჰაჰაჰაჰაჰ</span></center>
+
+
+
+
+
+
+</div>
+<paper-button raised>თარიღის კალკულატორი</paper-button>
+
+
+
+
+</body>
+</html>
