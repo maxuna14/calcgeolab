eebea9b012a5f4fa5b69c9694fde6a15d172f12e
Makho <maxuna15@mail.ru>
2015-12-15 16:25:05 +0400

    design update

---
+++ stopwatch.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>წამზომი</title>
+	<style type="text/css">
+		input {
+    padding:4px;}
+
+input[type="text"] {
+    width:50px;
+    text-align:center;}    
+
+div {
+    font-family:Arial,Verdana;
+    font-size:12px;
+    width:360px;
+    background-color:#f2f2f2;
+    padding:16px;
+    border:solid 1px #e5e5e5;
+    text-align:center;
+    margin:0 0 16px 0;}
+
+div span:last-child {
+color:green;}
+
+h1 {
+    font-size:14px;
+    margin:0 0 8px 0;
+}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="js/stopwatch.js"></script>
+</head>
+<body>
+<script>
+	/*
+
+JQUERY: STOPWATCH & COUNTDOWN
+
+This is a basic stopwatch & countdown plugin to run with jquery. Start timer, pause it, stop it or reset it. Same behaviour with the countdown besides you need to input the countdown value in seconds first. At the end of the countdown a callback function is invoked.
+
+Any questions, suggestions? marc.fuehnen(at)gmail.com
+
+*/
+
+$(document).ready(function() {
+
+    (function($){
+    
+        $.extend({
+            
+            APP : {                
+                
+                formatTimer : function(a) {
+                    if (a < 10) {
+                        a = '0' + a;
+                    }                              
+                    return a;
+                },    
+                
+                startTimer : function(dir) {
+                    
+                    var a;
+                    
+                    // save type
+                    $.APP.dir = dir;
+                    
+                    // get current date
+                    $.APP.d1 = new Date();
+                    
+                    switch($.APP.state) {
+                            
+                        case 'pause' :
+                            
+                            // resume timer
+                            // get current timestamp (for calculations) and
+                            // substract time difference between pause and now
+                            $.APP.t1 = $.APP.d1.getTime() - $.APP.td;                            
+                            
+                        break;
+                            
+                        default :
+                            
+                            // get current timestamp (for calculations)
+                            $.APP.t1 = $.APP.d1.getTime(); 
+                            
+                            // if countdown add ms based on seconds in textfield
+                            if ($.APP.dir === 'cd') {
+                                $.APP.t1 += parseInt($('#cd_seconds').val())*1000;
+                            }    
+                        
+                        break;
+                            
+                    }                                   
+                    
+                    // reset state
+                    $.APP.state = 'alive';   
+                    $('#' + $.APP.dir + '_status').html('Running');
+                    
+                    // start loop
+                    $.APP.loopTimer();
+                    
+                },
+                
+                pauseTimer : function() {
+                    
+                    // save timestamp of pause
+                    $.APP.dp = new Date();
+                    $.APP.tp = $.APP.dp.getTime();
+                    
+                    // save elapsed time (until pause)
+                    $.APP.td = $.APP.tp - $.APP.t1;
+                    
+                    // change button value
+                    $('#' + $.APP.dir + '_start').val('Resume');
+                    
+                    // set state
+                    $.APP.state = 'pause';
+                    $('#' + $.APP.dir + '_status').html('Paused');
+                    
+                },
+                
+                stopTimer : function() {
+                    
+                    // change button value
+                    $('#' + $.APP.dir + '_start').val('Restart');                    
+                    
+                    // set state
+                    $.APP.state = 'stop';
+                    $('#' + $.APP.dir + '_status').html('Stopped');
+                    
+                },
+                
+                resetTimer : function() {
+
+                    // reset display
+                    $('#' + $.APP.dir + '_ms,#' + $.APP.dir + '_s,#' + $.APP.dir + '_m,#' + $.APP.dir + '_h').html('00');                 
+                    
+                    // change button value
+                    $('#' + $.APP.dir + '_start').val('Start');                    
+                    
+                    // set state
+                    $.APP.state = 'reset';  
+                    $('#' + $.APP.dir + '_status').html('Reset & Idle again');
+                    
+                },
+                
+                endTimer : function(callback) {
+                   
+                    // change button value
+                    $('#' + $.APP.dir + '_start').val('Restart');
+                    
+                    // set state
+                    $.APP.state = 'end';
+                    
+                    // invoke callback
+                    if (typeof callback === 'function') {
+                        callback();
+                    }    
+                    
+                },    
+                
+                loopTimer : function() {
+                    
+                    var td;
+                    var d2,t2;
+                    
+                    var ms = 0;
+                    var s  = 0;
+                    var m  = 0;
+                    var h  = 0;
+                    
+                    if ($.APP.state === 'alive') {
+                                
+                        // get current date and convert it into 
+                        // timestamp for calculations
+                        d2 = new Date();
+                        t2 = d2.getTime();   
+                        
+                        // calculate time difference between
+                        // initial and current timestamp
+                        if ($.APP.dir === 'sw') {
+                            td = t2 - $.APP.t1;
+                        // reversed if countdown
+                        } else {
+                            td = $.APP.t1 - t2;
+                            if (td <= 0) {
+                                // if time difference is 0 end countdown
+                                $.APP.endTimer(function(){
+                                    $.APP.resetTimer();
+                                    $('#' + $.APP.dir + '_status').html('Ended & Reset');
+                                });
+                            }    
+                        }    
+                        
+                        // calculate milliseconds
+                        ms = td%1000;
+                        if (ms < 1) {
+                            ms = 0;
+                        } else {    
+                            // calculate seconds
+                            s = (td-ms)/1000;
+                            if (s < 1) {
+                                s = 0;
+                            } else {
+                                // calculate minutes   
+                                var m = (s-(s%60))/60;
+                                if (m < 1) {
+                                    m = 0;
+                                } else {
+                                    // calculate hours
+                                    var h = (m-(m%60))/60;
+                                    if (h < 1) {
+                                        h = 0;
+                                    }                             
+                                }    
+                            }
+                        }
+                      
+                        // substract elapsed minutes & hours
+                        ms = Math.round(ms/100);
+                        s  = s-(m*60);
+                        m  = m-(h*60);                                
+                        
+                        // update display
+                        $('#' + $.APP.dir + '_ms').html($.APP.formatTimer(ms));
+                        $('#' + $.APP.dir + '_s').html($.APP.formatTimer(s));
+                        $('#' + $.APP.dir + '_m').html($.APP.formatTimer(m));
+                        $('#' + $.APP.dir + '_h').html($.APP.formatTimer(h));
+                        
+                        // loop
+                        $.APP.t = setTimeout($.APP.loopTimer,1);
+                    
+                    } else {
+                    
+                        // kill loop
+                        clearTimeout($.APP.t);
+                        return true;
+                    
+                    }  
+                    
+                }
+                    
+            }    
+        
+        });
+          
+        $('#sw_start').live('click', function() {
+            $.APP.startTimer('sw');
+        });    
+
+        $('#cd_start').live('click', function() {
+            $.APP.startTimer('cd');
+        });           
+        
+        $('#sw_stop,#cd_stop').live('click', function() {
+            $.APP.stopTimer();
+        });
+        
+        $('#sw_reset,#cd_reset').live('click', function() {
+            $.APP.resetTimer();
+        });  
+        
+        $('#sw_pause,#cd_pause').live('click', function() {
+            $.APP.pauseTimer();
+        });                
+                
+    })(jQuery);
+        
+});
+
+</script>
+
+<div>
+    <h1>Stopwatch</h1>
+    <span id="sw_h">00</span>:
+    <span id="sw_m">00</span>:
+    <span id="sw_s">00</span>:
+    <span id="sw_ms">00</span>
+    <br/>
+    <br/>
+    <input type="button" value="Start" id="sw_start" />
+    <input type="button" value="Pause" id="sw_pause" />
+    <input type="button" value="Stop"  id="sw_stop" />
+    <input type="button" value="Reset" id="sw_reset" />
+    <br/>
+    <br/>
+    <span id="sw_status">Idle</span>
+</div>
+<div>
+    Don't run them at the same time ... it's a demo :-)
+</div>
+<div>
+    <h1>Countdown</h1>
+    <span id="cd_h">00</span>:
+    <span id="cd_m">00</span>:
+    <span id="cd_s">00</span>:
+    <span id="cd_ms">00</span>
+    <br/>
+    <br/>
+    <input type="button" value="Start" id="cd_start" />
+    <input type="button" value="Pause" id="cd_pause" />
+    <input type="button" value="Stop"  id="cd_stop" />
+    <input type="button" value="Reset" id="cd_reset" />
+    <br/>
+    <br/>
+    <input type="text" value="15" id="cd_seconds" />
+    secs
+    <br/>
+    <br/>
+    <span id="cd_status">Idle</span>
+</div>
+
+
+</body>
+</html>
---
+++ worldclock.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>მსოფლიო დრო</title>
+	<link rel="stylesheet" href="css/stylewd.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="bower_components/material-design-lite/material.min.css">
+	<script src="bower_components/material-design-lite/material.min.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+
+
+
+
+
+<div class="header">
+<!-- ლოგო -->
+<div class="logo"><img src="images/logo.png"></div>
+<!-- ახლადელი დრო -->
+<div class="rd-box">
+	<div class="rd-inner">
+		<h2 class="t-sq" id="current">
+      <a href="/worldclock/" title="The World Clock / Time Zones">თარიღი დრო</a>
+    </h2>
+    <a href="/worldclock/georgia/tskhinvali" id="clk_box" title="Current time in Tskhinvali">
+      <span id="clk_hm"><?php date_default_timezone_set("Asia/Tbilisi");
+echo date("G");?>:<?php date_default_timezone_set("Asia/Tbilisi");
+echo date("i");?></span>
+      <span id="clk_s">:<span id="ij0"><?php date_default_timezone_set("Asia/Tbilisi");
+echo date("s");?></span></span>
+      <span id="clk_am"></span><span id="clk_line"> </span>
+    </a>
+    <p><span id="ij1"><?php date_default_timezone_set("Asia/Tbilisi");
+echo date("l");?></span><br><span id="ij2"><?php date_default_timezone_set("Asia/Tbilisi");
+echo date("j");?> <?php date_default_timezone_set("Asia/Tbilisi");
+echo date("F");?> <?php date_default_timezone_set("Asia/Tbilisi");
+echo date("Y");?></span><br>
+	</div>
+</div>
+
+<!-- ნავიგაციააა აააააქ  -->
+  <div class="nav">
+  <!-- Accent-colored raised button with ripple -->
+  <a href="/"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">მთავარი</button></a>
+  <a href="about-us.php"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">ჩვენს შესახებ</button></a>
+  <a href="allcalc.php"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">კალკულატორები</button></a>
+  <a href="converters.php"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">კონვერტერები</button></a>
+  <a href="newuser.php"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">რეგისტრაცია/ავტორიზაცია</button></a>
+  <a href="contact.php"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"> კონტაქტი</button></a>
+</div>
+<!-- აქამდეეეეეეეეეეეეეეეეეეეეე -->
+
+
+
+</div>
+	
+
+    
+<div class="konteineri">
+<center><h1>მსოფლიო დრო</h1></center>
+<div class="row">
+  <div class="col-sm-3">
+  	<table class="table table-hover">
+	<tr>
+		<th>ქალაქი</th>
+		<th>დრო</th>
+	</tr>
+	<tr>
+  		<td>თბილისი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>მოსკოვი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>კიევი</td>
+  		<td>15:59</td>
+	</tr>
+</table>
+  </div>
+  <div class="col-sm-3">
+  	<table class="table table-hover">
+	<tr>
+		<th>ქალაქი</th>
+		<th>დრო</th>
+	</tr>
+	<tr>
+  		<td>თბილისი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>მოსკოვი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>კიევი</td>
+  		<td>15:59</td>
+	</tr>
+</table>
+  </div>
+  <div class="col-sm-3">
+  	<table class="table table-hover">
+	<tr>
+		<th>ქალაქი</th>
+		<th>დრო</th>
+	</tr>
+	<tr>
+  		<td>თბილისი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>მოსკოვი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>კიევი</td>
+  		<td>15:59</td>
+	</tr>
+</table>
+  </div>
+  <div class="col-sm-3">
+  	<table class="table table-hover">
+	<tr>
+		<th>ქალაქი</th>
+		<th>დრო</th>
+	</tr>
+	<tr>
+  		<td>თბილისი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>მოსკოვი</td>
+  		<td>15:59</td>
+	</tr>
+	<tr>
+  		<td>კიევი</td>
+  		<td>15:59</td>
+	</tr>
+</table>
+  </div>
+</div>
+
+
+
+
+
+</div>
+<div class="footer">
+	<center><span id="footer">დიადი და დიდებული ავტორის ინფო წავა ააააააქ ჰაჰაჰაჰაჰაჰაჰ</span></center>
+<div class="row">
+  <div class="col-sm-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error natus maiores et placeat saepe modi soluta ipsa quisquam. Iste accusamus voluptatum dolorum odit aperiam facilis quam reprehenderit voluptatem praesentium eos.</div>
+  <div class="col-sm-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque hic veniam veritatis iure autem fugiat ut ipsum eaque, ratione quibusdam, ipsam minima, quidem incidunt. Impedit qui nam, consectetur amet dolore!</div>
+  <div class="col-sm-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid deleniti illo provident recusandae sint accusantium. Iusto explicabo nostrum, dolorem eius ipsa, molestias ea nisi deserunt temporibus eveniet alias repellendus suscipit.</div>
+</div>
+
+
+
+
+
+</div>
+
+</body>
+</html>
